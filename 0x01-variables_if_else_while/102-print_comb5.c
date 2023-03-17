acac07a83019c52af584c7b8c42b3f8c743f61a5
sara <saraelshobaky2000@gmail.com>
2023-03-17 16:31:25 -0700

    last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,13 +12,13 @@ int main(void)
 
 	for (a = 0; a <= 98; a++)
 	{
-		for (b = a + 1; b <=99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			putchar((a / 10)+'0');
-			putchar((a % 10)+'0');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar((b / 10)+'0');
-			putchar((b % 10)+'0');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 			if (a == 98 && b == 99)
 				continue;
 			putchar(',');
