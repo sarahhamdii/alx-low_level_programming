612cc74b085c585ea23c7152f9dcd591c8c13071
sara <saraelshobaky2000@gmail.com>
2023-03-17 16:07:11 -0700

    11 task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = a + 1; b <= 57; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
