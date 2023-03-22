cbb69f24d363d949e6a1daa9ed99162a1b18204b
sara <saraelshobaky2000@gmail.com>
2023-03-21 18:18:07 -0700

    10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - check
+ * @n1: first number
+ * @n2: second number
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
