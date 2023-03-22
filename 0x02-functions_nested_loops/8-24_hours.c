d9f2672e79424b72b8086ea1ad963caaddc0a4b7
sara <saraelshobaky2000@gmail.com>
2023-03-21 17:14:04 -0700

    8 task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - check
+ * @n: minute of the day
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					_putchar (a);
+					_putchar (b);
+					_putchar (':');
+					_putchar (c);
+					_putchar (d);
+					_putchar ('\n')
+				}
+			}
+		}
+	}
+}
