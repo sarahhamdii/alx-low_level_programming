3d9906dc919ef69ddc2e9406b940a87f56241807
sara <saraelshobaky2000@gmail.com>
2023-03-23 10:40:13 -0700

    5 task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 14; x++)
+	{
+		_putchar ("%d", x);
+	}
+	_putchar ("\n");
+}
