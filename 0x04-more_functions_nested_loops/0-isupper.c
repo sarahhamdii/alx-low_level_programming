679395dd79cfdb1409e27edb21bcfbb9478814b1
sara <saraelshobaky2000@gmail.com>
2023-03-23 07:58:58 -0700

    zero task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checked
+ *
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65; c<= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
