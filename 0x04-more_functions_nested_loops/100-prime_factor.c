ac100c7ad7dbd99654f3b44db117c5f43db6ee91
sara <saraelshobaky2000@gmail.com>
2023-03-23 14:41:25 -0700

    11 task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,17 +9,24 @@
 
 int main(void)
 {
-        long x, maxf;
-        long number = 612852475143;
-        double square = sqrt(number);
-
-        for (x = 1; x <= square; x++)
-        {
-                if (number % x == 0)
-                {
-                        maxf = number / x;
-                }
-        }
-        printf("%ld\n", maxf);
-        return (0);
+        long int n;
+	n = 612852475143;
+	long int div=2, ans = 0, maxFact;
+	while(n!=0)
+	{
+		if(n % div !=0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if(n == 1)
+			{
+				printf("%d is the largest prime factor !",maxFact);
+				ans = 1;
+				break;
+			}
+		}
+	}
+	return 0;
 }
