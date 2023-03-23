f9a992b06d0b7de0c44f0ab4111794160a0558bb
sara <saraelshobaky2000@gmail.com>
2023-03-23 13:06:09 -0700

    9 task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x < 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
 		if (x % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((x % 3 == 0) && (x % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
