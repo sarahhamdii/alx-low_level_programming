6c677a76fb348a22c748961e83909eb5949de462
sara <saraelshobaky2000@gmail.com>
2023-03-23 08:34:31 -0700

    2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
