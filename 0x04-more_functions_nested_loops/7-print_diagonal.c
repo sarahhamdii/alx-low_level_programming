7ed63164bda27b2c84fd5bed32522374814d3631
sara <saraelshobaky2000@gmail.com>
2023-03-23 11:18:19 -0700

    7 task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: number
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('\');
+	}
+	_putchar('\n');
+}
