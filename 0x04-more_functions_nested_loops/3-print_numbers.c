34ad67b465e5152aa9af7e6220a567b7c726c7f3
sara <saraelshobaky2000@gmail.com>
2023-03-23 08:47:41 -0700

    3 task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar (x);
+	}
+	_putchar ('\n');
+}
