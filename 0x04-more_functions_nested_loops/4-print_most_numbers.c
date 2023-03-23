2248653ad9a4ca6a5a67730dca2659c6e5c81b71
sara <saraelshobaky2000@gmail.com>
2023-03-23 10:01:51 -0700

    4 task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if ( c == 50 || c == 52)
+		{
+			continue
+		}
+		else
+		{
+			_putchar (c);
+		}
+	}
+	_putchar ('\n')
+}
