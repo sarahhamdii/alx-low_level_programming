a86bd2910647b2cab3fbd1d1f03a155eb1e5617f
sara <saraelshobaky2000@gmail.com>
2023-03-23 11:54:57 -0700

    7 task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,11 +18,11 @@ void print_diagonal(int n)
 			_putchar(' ');
 		}
 	}
-		if (n <= 0)
-			_putchar('\n');
-		else
-		{
-			_putchar('\\');
-			_putchar('\n');
-		}
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
