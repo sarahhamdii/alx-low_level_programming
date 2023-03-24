433ce9b7659b52dae1cdf6d89526e004221558ae
sara <saraelshobaky2000@gmail.com>
2023-03-24 10:46:28 -0700

    11 task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -6,27 +5,18 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-        long int n;
-	n = 612852475143;
-	long int div=2, ans = 0, maxFact;
-	while(n!=0)
+	long i, n = 612852475143;
+
+	for (i  = 2; i <= n; i++)
 	{
-		if(n % div !=0)
-			div = div + 1;
-		else
+		if (n % i == 0)
 		{
-			maxFact = n;
-			n = n / div;
-			if(n == 1)
-			{
-				printf("%d is the largest prime factor !",maxFact);
-				ans = 1;
-				break;
-			}
+			n = n / i;
+			i--;
 		}
 	}
-	return 0;
+	printf("%lu\n", i);
+	return (0);
 }
