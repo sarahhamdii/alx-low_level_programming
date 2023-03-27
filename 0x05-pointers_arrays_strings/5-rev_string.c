f9cb1516aa79d809509f3e21c30e31ef3b0f0599
sara <saraelshobaky2000@gmail.com>
2023-03-27 12:01:12 -0700

    5 task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,15 @@ void rev_string(char *s)
 {
 	char ch, *p, *q;
 
-	for (q = s; *q != '\0'; ++q);
-	 if (q > s)
-		 --q;
-	 for (p = s; p < q; ++p, --q)
-	 {
-		 ch = *p;
-		 *p = *q;
-		 *q = ch;
-	 }
-	 return s;
+	for (q = s; *q != '\0'; ++q)
+		;
+       	if (q > s)
+	       	--q;
+	for (p = s; p < q; ++p, --q)
+       	{
+		ch = *p;
+		*p = *q;
+		*q = ch;
+	}
+	return s;
 }
