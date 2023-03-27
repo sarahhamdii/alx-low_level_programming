26c432f6ee65494169d2edc12b799ba75294dd0f
sara <saraelshobaky2000@gmail.com>
2023-03-27 12:29:56 -0700

    7 task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int x;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		x = (i - 1) / 2;
+		x += 1;
+	}
+	else
+	{
+		x = i / 2;
+	}
+	for (; x < i; x++)
+	{
+		_putchar(str[x]0);
+	}
+	_putchar('\n');
+}
