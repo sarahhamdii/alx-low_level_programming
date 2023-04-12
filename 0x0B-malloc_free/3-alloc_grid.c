1269256766a7ca04d2f729e42d1ce02e9ee16d34
sara <saraelshobaky2000@gmail.com>
2023-04-12 10:56:19 -0700

    3 task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int w;
+	int h;
+	char **ar;
+
+	if (width <= 0 && height <= 0)
+		return (NULL);
+
+	ar = (int **)malloc(sizeof(int *) * height);
+	
+	if (ar == NULL)
+		return (NULL);
+	for (w = 0; w < width; w++)
+		free(ar[w]);
+	for (h = 0; h < hight; h++)
+		free(ar[h]);
+	free (ar);
+}
