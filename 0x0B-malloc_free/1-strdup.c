04a6354b700fc973bfd1a44f12cc7c1564ea0d44
sara <saraelshobaky2000@gmail.com>
2023-04-11 17:29:13 -0700

    1 task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - write a function that returns a pointer
+ *
+ * Return: NULL
+ */
+
+char *_strdup(char *str);
+{
+	unsigned int size, i;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	s = mallco(i* sizeof(char) +1);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i]=str[i];
+	}
+	return (s);
+}
