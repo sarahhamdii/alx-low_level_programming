aa6c9d9a9462d81934f2a839357e798ff3f12706
sara <saraelshobaky2000@gmail.com>
2023-04-12 10:07:17 -0700

    1task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,20 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - write a function that returns a pointer
- *
+ * @str: string
  * Return: NULL
  */
 
 char *_strdup(char *str)
 {
-	int size = 0 , i;
+	int size = 0;
+	int i;
 	char *s;
 
 	if (str == NULL)
 		return (NULL);
+
+	while (str[size])
+	{
+		size++;
+	}
 
 	s = (char *)malloc(size + 1);
 
