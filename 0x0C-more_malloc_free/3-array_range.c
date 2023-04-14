10951fe6616123b1dda9b4775eb86813caddb6b2
sara <saraelshobaky2000@gmail.com>
2023-04-13 20:02:22 -0700

    last task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(int) * (min - max + 1);
+	ar = malloc(sizeof(int) * (min - max + 1));
 
 	if (ar == NULL)
 		return (NULL);
