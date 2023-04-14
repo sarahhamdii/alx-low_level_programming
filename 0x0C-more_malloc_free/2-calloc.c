9d7855a5a87749dda7d5730271761038adb01971
sara <saraelshobaky2000@gmail.com>
2023-04-13 19:33:24 -0700

    2 task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,10 +16,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ar;
 	unsigned int i;
 
-	ar = malloc(nmemb * size);
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	ar = malloc(nmemb * size);
 
 	if (ar == NULL)
 		return (NULL);
