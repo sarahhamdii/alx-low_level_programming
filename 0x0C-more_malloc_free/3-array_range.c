106398ed526a67b66285f3d19e9fc94a1282b7d3
sara <saraelshobaky2000@gmail.com>
2023-04-13 19:57:06 -0700

    last task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ ** array_range - prints buffer in hexa
+ * @min: small number
+ * @max: large number
+ *
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+	char *ar;
+
+	if (min > max)
+		return (NULL);
+
+	ar = malloc(sizeof(int) * (min - max +1);
+
+	if (ar == NULL)
+		return (NULL);
+
+	return (ar);
+}
