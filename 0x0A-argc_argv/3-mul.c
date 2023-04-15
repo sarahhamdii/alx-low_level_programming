6f0c666ae73155d25f3a4042382aa96a2d85a149
sara <saraelshobaky2000@gmail.com>
2023-04-15 12:44:43 -0700

    3 task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print it's name
+ * @argc: number
+ * @argv: array
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
