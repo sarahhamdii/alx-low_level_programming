f7594a178f77c9101f2c0396f478f3929aaf7648
sara <saraelshobaky2000@gmail.com>
2023-04-15 18:09:57 -0700

    last task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print it's name
+ * @argc: number
+ * @argv: array
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+	int n;
+	
+	if (argc == 1)
+	{
+		printf("0\n");
+		return(0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (n = 0; argv[n][i] >= 48 && argv[n][i] <= 57; n++)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (argc > 1)
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
