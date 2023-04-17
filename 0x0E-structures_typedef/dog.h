e563174cbf42d26a57c988c1b57b623658ad426d
sara <saraelshobaky2000@gmail.com>
2023-04-17 12:54:08 -0700

    0 task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef my_dog
+#define my_dog
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * typedef struct - check the code
+ * @name: first character
+ * @owner: second character
+ * @age: third character
+ *
+ * Return: Always 0.
+ */
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	char age
+} dog;
+
+#endif
