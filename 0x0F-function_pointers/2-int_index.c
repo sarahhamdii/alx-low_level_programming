d9544e17c890b43be19db51f5e96095f1fff3181
sara <saraelshobaky2000@gmail.com>
2023-04-19 14:31:52 -0700

    2 task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/*
+ * int_index - searches for an integer
+ * @array: array
+ * @size: number of elements in the array
+ * @cmp: pointer
+ *
+ * Return: 0 or -1 
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				cmp(array[i]);
+				return (i);
+			}
+		}
+		else
+			return (-1);
+	}
+}
