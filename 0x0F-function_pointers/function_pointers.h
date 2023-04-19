99d20e46686b9f90b5746d91c79f6b29b54e860d
sara <saraelshobaky2000@gmail.com>
2023-04-19 13:35:06 -0700

    0 task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,7 @@
+#ifndef FUNCTION_H
+
+#define FUNCTION_H
+
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
@@ -5,3 +9,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
