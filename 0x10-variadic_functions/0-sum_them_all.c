3ffd1500fea9cf2aefc0ad649d88c6e398aedab4
sara <saraelshobaky2000@gmail.com>
2023-04-20 10:48:06 -0700

    0 task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - calculate summation
+ * @n: constant number
+ *
+ * Return: Always 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list a;
+
+	if (n == 0)
+		return (0);
+
+	va_start(a, n)
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(a, int);
+	}
+	va_end(a);
+	return (sum);
+}
