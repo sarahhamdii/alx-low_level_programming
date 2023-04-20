aad109e2e76e114d1617f252653e15e8e7c16cb7
sara <saraelshobaky2000@gmail.com>
2023-04-19 18:38:01 -0700

    last task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int a, int b);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	func = get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error\n");
+		exit (99);
+	}
+	if (((argv[2][0] == '/') || (argv[2][0] == '%')) && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
