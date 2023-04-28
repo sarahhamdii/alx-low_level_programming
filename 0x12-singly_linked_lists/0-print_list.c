1cdc1472573d8d651c6da3914d4844becc064a7b
sara <saraelshobaky2000@gmail.com>
2023-04-28 13:55:04 -0700

    0 task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,10 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++)
 	{
 		if (h->str == NULL)
-			printf("[0](nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
 	}
-	return(i);
+	return (i);
 }
