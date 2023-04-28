e54aca7fa42a7d33e44f95c56f9d6fe6a9401a43
sara <saraelshobaky2000@gmail.com>
2023-04-28 13:52:05 -0700

    0 task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - print
+ * @h: constant pointer
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[0](nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return(i);
+}
