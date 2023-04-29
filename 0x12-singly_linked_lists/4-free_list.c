bb10780b464678ae4023a4e114e7ccd1cd686d9a
sara <saraelshobaky2000@gmail.com>
2023-04-28 19:33:01 -0700

    last task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - free
+ * @head: pointer
+ *
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	free_list(head->next);
+	if (head->str)
+		free(head->str);
+	free(head);
+}
