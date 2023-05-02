3369717410734ca2c442d08612c4ddf4897bd9b6
sara <saraelshobaky2000@gmail.com>
2023-05-01 22:26:31 -0700

    0 task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - print
+ * @h: constant pointer
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const listint_t *h)
+{	
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+			printf("[%i] $s\n", h->n);
+			h = h->next;
+	}
+	return (i);
+}
