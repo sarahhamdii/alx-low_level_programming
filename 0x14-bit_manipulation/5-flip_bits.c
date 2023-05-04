fafa163f10929b3f10171aee3e40d9a54d59ff1b
sara <saraelshobaky2000@gmail.com>
2023-05-04 13:35:32 -0700

    last task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits
+ * @n: long integer
+ * @m: long integer
+ *
+ * Return: the number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int z;
+	int num;
+
+	z = n ^ m;
+
+	for (num = 0; z != 0; z = z >> 1)
+	{
+		num += z & 1;
+	}
+	return (num);
+}
