f72f007f69cc0cb4e18543c8d880d11b0f5a1b0e
sara <saraelshobaky2000@gmail.com>
2023-05-04 07:50:26 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,17 +13,15 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 	unsigned int num = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[i])
 	{
-		if (b == NULL)
-			return (0);
-
-		if (b[i] < 48 || b[i] > 49)
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		num = num << 1;
 		num = num + (b[i] - '0');
 		i++;
-
 	}
 	return (num);
 }
