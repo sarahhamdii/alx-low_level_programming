754e18f670427237b207fed0b418067f6e6b217e
sara <saraelshobaky2000@gmail.com>
2023-05-08 18:31:39 -0700

    2 task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -19,9 +19,8 @@ char *_strchr(char *s, char c)
 		{
 			return (s + i);
 		}
+		if (c == '\0')
+			return (s + i);
 	}
-	if (c == '\0')
-		return (s + i);
-	else
-		return (NULL);
+	return (NULL);
 }
