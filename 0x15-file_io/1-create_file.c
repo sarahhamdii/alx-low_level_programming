3fd4a29eba4cdf2ad75cfc59dc1e65d8dce8a25f
sara <saraelshobaky2000@gmail.com>
2023-05-08 17:57:59 -0700

    1 task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,14 +18,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
-		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-		if (fd == -1)
-			return (-1);
 		l = write(fd, text_content, strlen(text_content));
 		if (l == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
