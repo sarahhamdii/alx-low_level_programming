a8b3d7477a31ba97e3a1cbbfe20496b153172ec2
sara <saraelshobaky2000@gmail.com>
2023-05-08 17:03:37 -0700

    0 task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	b = malloc(letters);
+	b = malloc(letters + 1);
 	if (b == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
