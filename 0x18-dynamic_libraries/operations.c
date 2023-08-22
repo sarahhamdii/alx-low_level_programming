63b5d9057cbf4022dd6385f32dde318484bf1b7a
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-21 17:15:01 -0700

    3 task

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - add numbers
+ * @x: first number
+ * @y: second number
+ * Return: the result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - sub numbers
+ * @x: first number
+ * @y: second number
+ * Return: the result
+ */
+int sub(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * mul - mul numbers
+ * @x: first number
+ * @y: second number
+ * Return: the result
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * div - div numbers
+ * @x: first number
+ * @y: second number
+ * Return: the result
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * mod - mod numbers
+ * @x: first number
+ * @y: second number
+ * Return: the result
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
